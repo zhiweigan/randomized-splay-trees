2acdadb6e259d30cd76d7b407ae616ccbaf39e92
stronto9 <pkrishna@mit.edu>
2020-12-07 03:08:27 -0500

    Moved class definitions

--- trees/trees.h
+++ trees/trees.h
@@ -54,22 +54,6 @@ public:
     s* Search(int key);
 };
 
-// Uncomment ONLY the ones you want to implement
-class TemplateSplayTree : public SplayTree
-{
-//private:
-//    s* RR_Rotate(s* k2);
-//    s* LL_Rotate(s* k2);
-//    s* Splay(int key, s* root);
-//    s* New_Node(int key);
-//public:
-//    s* Insert(int key, s* root);
-//    s* Delete(int key, s* root);
-//    s* Search(int key, s* root);
-};
-
-#endif //RANDOMIZED_SPLAY_TREES_TREES_H
-
 class ThreeRotSplayTree : public SplayTree
 {
     private:
@@ -102,3 +86,19 @@ class FourRotSplayTree : public SplayTree
     }
     s* Search(int key);
 }
+
+// Uncomment ONLY the ones you want to implement
+class TemplateSplayTree : public SplayTree
+{
+//private:
+//    s* RR_Rotate(s* k2);
+//    s* LL_Rotate(s* k2);
+//    s* Splay(int key, s* root);
+//    s* New_Node(int key);
+//public:
+//    s* Insert(int key, s* root);
+//    s* Delete(int key, s* root);
+//    s* Search(int key, s* root);
+};
+
+#endif //RANDOMIZED_SPLAY_TREES_TREES_H
