bda4454c103833c5d69dda2a5ea331f45c5dcffa
stronto9 <pkrishna@mit.edu>
2020-12-09 20:24:32 -0500

    Bug fix scheme 3

--- trees/scheme3.cpp
+++ trees/scheme3.cpp
@@ -20,8 +20,12 @@ void Scheme3_2Rot::AccessSplay(s *x)
 {
     while (x->parent) {
         int action = this->choice(this->generator);
-        if (((action == 0) != this->invert) && x->parent->parent){
-            x = x->parent->parent;
+        if (((action == 0) != this->invert)){
+            if (x->parent->parent) {
+                x = x->parent->parent;
+            } else {
+                x = x->parent;
+            }
             continue;
         }
 
